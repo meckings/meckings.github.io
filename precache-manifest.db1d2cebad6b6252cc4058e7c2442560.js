02babf3522ceb52e5ed777afb1f6b8c18d1c89fa
Ebuka Maduekwe <ebuka.maduekwe@interswitchgroup.com>
2020-10-11 18:32:10 +0100

    long initial commit

---
+++ precache-manifest.db1d2cebad6b6252cc4058e7c2442560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7449d1bc383b78ad71c9b87952c7ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01c1fa1af5e60ea04b3",
+    "url": "/static/css/main.ed022b04.chunk.css"
+  },
+  {
+    "revision": "02efe0f3df265dce6786",
+    "url": "/static/js/2.17beb61f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17beb61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01c1fa1af5e60ea04b3",
+    "url": "/static/js/main.86093832.chunk.js"
+  },
+  {
+    "revision": "ca387a443284ba7dc1ef",
+    "url": "/static/js/runtime-main.c760f0c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
