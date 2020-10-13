a532fe65902996bb595e2779c30a8ae5ca622717
Ebuka Maduekwe <ebuka.maduekwe@interswitchgroup.com>
2020-10-13 17:41:04 +0100

    refactored to render properly on mobile

---
+++ precache-manifest.b3770db7df2c7f84912ef480ae10f3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a30eb4ff972baa47e63665060b9d7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43cb3d178907041bd2be",
+    "url": "/static/css/main.13a1e66f.chunk.css"
+  },
+  {
+    "revision": "fdbf5080d99d637d1389",
+    "url": "/static/js/2.83f05358.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.83f05358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43cb3d178907041bd2be",
+    "url": "/static/js/main.2c589b46.chunk.js"
+  },
+  {
+    "revision": "ca387a443284ba7dc1ef",
+    "url": "/static/js/runtime-main.c760f0c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc41671bec0c2bddb6789b48558fe1c7.js"
+  "/precache-manifest.b3770db7df2c7f84912ef480ae10f3f8.js"
 );
 
 self.addEventListener('message', (event) => {
